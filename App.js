42caef24a98f2d435face55bb6bbe549ded1d4af
Aditya Kumar <adityakumar9939@gmail.com>
2024-02-26 02:01:09 +0530

    wrote some css

--- App.js
+++ App.js
@@ -1,16 +1,91 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestaurantContainer
+ *     - RestaurantCard
+ *       - Img
+ *       - Name of Restaurant, Star rating, cuisine, delevery time.
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
 
-// JSX => Babel transpiled it to React.creatElement => ReactElement-JS Object => HTMLElements(render)
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-const jsxHeading = (
-    <h1 className="head" tabIndex="5">
-        Created using JSX
-    </h1>
-);
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+            <img
+                className="res-logo"
+                alt="res=logo"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvjeztqzunmmcugnbgqt"
+            />
+            <h3>Aditya's food</h3>
+            <h3>All Panir dishes, North cuisine</h3>
+            <h3>5 Star </h3>
+            <h3>30 mins</h3>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<AppLayout />);
